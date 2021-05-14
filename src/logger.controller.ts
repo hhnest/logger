375d0d695529fbbcce958aff404a0cbbbde7bc20
François ACHACHE <francois.achache@corobor.com>
2021-05-14 23:00:47 +0200

    add endpoint to get level of specific logger remove runtime dep

--- src/logger.controller.ts
+++ src/logger.controller.ts
@@ -17,26 +17,46 @@ export class LoggerController {
     return (Logger as any).logLevels;
   }
 
-  @Get(':level')
-  setLogLevelDebug(@Param('level') level: LogLevel) {
-    const levels: LogLevel[] = [];
-    switch (level) {
-      case 'verbose':
-        levels.push('verbose');
-      case 'debug':
-        levels.push('debug');
-      case 'log':
-        levels.push('log');
-      case 'warn':
-        levels.push('warn');
-      case 'error':
-        levels.push('error');
-        break;
-      default:
-        levels.push('error', 'warn', 'log', 'debug');
-    }
-    Logger.overrideLogger(levels);
-    return levels;
+  @Get('verbose')
+  setLogLevelVerbose() {
+    Logger.overrideLogger(['error', 'warn', 'log', 'debug', 'verbose']);
+    return (Logger as any).logLevels;
+  }
+
+  @Get('debug')
+  setLogLevelDebug() {
+    Logger.overrideLogger(['error', 'warn', 'log', 'debug']);
+    return (Logger as any).logLevels;
+  }
+
+  @Get('log')
+  setLogLevelLog() {
+    Logger.overrideLogger(['error', 'warn', 'log']);
+    return (Logger as any).logLevels;
+  }
+
+  @Get('warn')
+  setLogLevelWarn() {
+    Logger.overrideLogger(['error', 'warn']);
+    return (Logger as any).logLevels;
+  }
+
+  @Get('error')
+  setLogLevelError() {
+    Logger.overrideLogger(['error']);
+    return (Logger as any).logLevels;
+  }
+
+  @Get('reset')
+  resetLogLevel() {
+    Logger.overrideLogger(['error', 'warn', 'log', 'debug']);
+    return (Logger as any).logLevels;
+  }
+
+  @Get(':cls')
+  setLogLevelDebug2(@Param('cls') cls: string) {
+    const logger: LoggerService = this.moduleRef.get(`Log${cls}`);
+    return logger.levels || (Logger as any).logLevels;
   }
 
   @Get(':cls/:level')
