0d92545498571f536b5794a7cf9f9ed3fe93f708
François ACHACHE <francois.achache@corobor.com>
2021-06-05 21:10:06 +0200

    typo

--- manage-optional-dep.js
+++ manage-optional-dep.js
@@ -6,7 +6,7 @@ fs.readFile('./dist/logger.controller.js', 'utf8', (err,data) => {
     return console.log(err);
   }
   const result = data.replace(/const swagger_1 = require\("@nestjs\/swagger"\);/, `let swagger_1 = {ApiTags: (val) => null};
-try {swagger_1 = require("@nestjs/swagger2");} catch(e) {}`
+try {swagger_1 = require("@nestjs/swagger");} catch(e) {}`
   ).replace(/], LoggerController\);/, `].filter((d) => !!d), LoggerController);`);
   fs.writeFile('./dist/logger.controller.js', result, 'utf8', function (err) {
      if (err) return console.log(err);
