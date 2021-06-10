3567c78c4bdc06cb3cad64484e8677194cfe95fb
François ACHACHE <francois.achache@corobor.com>
2021-06-10 15:26:19 +0200

    add log level in log 2

--- src/logger.service.ts
+++ src/logger.service.ts
@@ -13,31 +13,31 @@ export class LoggerService extends Logger {
 
   error(message: any, trace?: string, context?: string): void {
     if (this.isLogLvlEnabled('error')) {
-      Logger.error(message, trace, `[ERROR  ] ${context || this.context}`);
+      Logger.error(message, trace, `ERROR   | ${context || this.context}`);
     }
   }
 
   warn(message: any, context?: string): void {
     if (this.isLogLvlEnabled('warn')) {
-      Logger.warn(message, `[WARN   ] ${context || this.context}`);
+      Logger.warn(message, `WARN    | ${context || this.context}`);
     }
   }
 
   log(message: any, context?: string): void {
     if (this.isLogLvlEnabled('log')) {
-      Logger.log(message, `[LOG    ] ${context || this.context}`);
+      Logger.log(message, `LOG     | ${context || this.context}`);
     }
   }
 
   debug(message: any, context?: string): void {
     if (this.isLogLvlEnabled('debug')) {
-      Logger.debug(message, `[DEBUG  ] ${context || this.context}`);
+      Logger.debug(message, `DEBUG   | ${context || this.context}`);
     }
   }
 
   verbose(message: any, context?: string): void {
     if (this.isLogLvlEnabled('verbose')) {
-      Logger.verbose(message, `[VERBOSE] ${context || this.context}`);
+      Logger.verbose(message, `VERBOSE | ${context || this.context}`);
     }
   }
 
